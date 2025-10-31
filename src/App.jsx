248349d431ae8aef09f7a792582d93dacbb15701
FathimaHasnakp <fathimahasna738@gmail.com>
2025-10-31 10:58:56 +0530

    committed

--- src/App.jsx
+++ src/App.jsx
@@ -5,47 +5,64 @@
 // import Welcome from './embedding-expression/welcome'
 
 
+import Props from "./Functional-components/Props"
 
-// function App() {
+// import State from "./Functional-components/State"
 
-//   return (
-//     <>
-    {/* {/----------Embedding-expression----------} */}
-    {/* <Welcome/> */}
-    {/* <Calling_function/> */}
-    {/* <Condition /> */}
-    {/* <RenderingList/> */}
+// import Basic from "./Functional-components/Basic"
 
-//     </>
-//   )
-// }
 
-// export default App
+
+function App() {
+
+  return (
+    <>
+{/* {/----------Embedding-expression----------} */ }
+{/* <Welcome/> */ }
+{/* <Calling_function/> */ }
+{/* <Condition /> */ }
+{/* <RenderingList/> */ }
+
+
+{/* {/*-------- functional components *---------------/} */}
+{/* <Basic/> */}
+{/* <State/> */}
+<Props/>
+    </>
+  )
+ }
+
+ export default App
+
+
+//------------ class components----------------------//
 
 // import Counter from './Class-components/Counter'
 // import UsingProps from './Class-components/UsingProps'
 // import Basic from './Class-components/Basic'
-import { Component } from "react";
-import UnMounting from "./Class-components/Life cycle methods/UnMounting";
+// import { Component } from "react";
+// import ButtonUnMount from "./Class-components/Life cycle methods/ButtonUnMount";
+// import UnMounting from "./Class-components/Life cycle methods/UnMounting";
 // import Updating from "./Class-components/Life cycle methods/Updating";
 // import Mounting from "./Class-components/Life cycle methods/Mounting";
 // import Wrkcounter from "./Class-components/Wrkcounter";
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-    {/* <Basic/> */}
-    {/* <UsingProps name="hasna"/> */}
-    {/* <Counter/> */}
-    {/* <Wrkcounter/> */}
-    {/* <Mounting/> */}
-{/* <Updating/> */}
-<UnMounting/>
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <div>
+        {/* <Basic/> */}
+        {/* <UsingProps name="hasna"/> */}
+        {/* <Counter/> */}
+        {/* <Wrkcounter/> */}
+        {/* <Mounting/> */}
+        {/* <Updating/> */}
+        {/* <UnMounting/> */}
+        // <ButtonUnMount />
 
-      </div>
-    )
-  }
-}
+//       </div>
+//     )
+//   }
+// }
 
 
---
+++ src/Functional-components/Basic.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Basic = () => {
+    let name = "hasna";
+  return (
+    <div>
+      <h1>Hello, {name}</h1>
+    </div>
+  )
+}
+
+export default Basic
